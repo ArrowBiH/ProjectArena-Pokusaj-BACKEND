3804c534967deff418c40ee80a3da6002eed5ad7
ArrowBiH <67058944+ArrowBiH@users.noreply.github.com>
2022-12-04 15:27:38 +0100

    Add files via upload

---
+++ lib/auth/screens/auth_screen.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'package:passwordfield/passwordfield.dart';
+
+import '../../common/widgets/custom_button.dart';
+import '../../common/widgets/custom_textfield.dart';
+import '../services/auth_service.dart';
+
+enum Auth {
+  signin,
+  signup,
+}
+
+class AuthScreen extends StatefulWidget {
+  static const String routeName = "/auth-screen";
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
+  final _signInFormKey = GlobalKey<FormState>();
+  final _signUpFormKey = GlobalKey<FormState>();
+  final AuthService authService = AuthService();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+  }
+
+  void signUpUser() {
+    authService.signUpUser(
+      context: context,
+      email: _emailController.text,
+      password: _passwordController.text,
+      name: _nameController.text,
+    );
+  }
+
+  void signInUser() {
+    authService.signInUser(
+      context: context,
+      email: _emailController.text,
+      password: _passwordController.text,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            "assets/images/logo2.png",
+            width: 400,
+          ),
+          ListTile(
+            tileColor: _auth == Auth.signup
+                ? Colors.white
+                : Color.fromARGB(193, 255, 255, 255),
+            title: const Text(
+              "Create Account",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: Radio(
+                activeColor: Colors.greenAccent,
+                value: Auth.signup,
+                groupValue: _auth,
+                onChanged: (Auth? val) {
+                  setState(() {
+                    _auth = val!;
+                  });
+                }),
+          ),
+          if (_auth == Auth.signup)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              color: Colors.white,
+              child: Form(
+                key: _signUpFormKey,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "E-mail",
+                      ),
+                    ),
+                    CustomTextField(
+                      controller: _emailController,
+                      hintText: "Email",
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Name",
+                      ),
+                    ),
+                    CustomTextField(
+                      controller: _nameController,
+                      hintText: "Name",
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Password",
+                      ),
+                    ),
+                    CustomTextField(
+                      controller: _passwordController,
+                      hintText: "Password",
+                    ),
+                    const SizedBox(height: 10),
+                    CustomButton(
+                        text: "Sign Up",
+                        onTap: () {
+                          signUpUser();
+                        }),
+                  ],
+                ),
+              ),
+            ),
+          ListTile(
+            title: const Text(
+              "Login",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: Radio(
+                activeColor: Colors.greenAccent,
+                value: Auth.signin,
+                groupValue: _auth,
+                onChanged: (Auth? val) {
+                  setState(() {
+                    _auth = val!;
+                  });
+                }),
+          ),
+          if (_auth == Auth.signin)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              color: Colors.white,
+              child: Form(
+                key: _signInFormKey,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "E-mail",
+                      ),
+                    ),
+                    CustomTextField(
+                      controller: _emailController,
+                      hintText: "Email",
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Password",
+                      ),
+                    ),
+                    PasswordField(
+                      controller: _passwordController,
+                      hintText: "Password",
+                    ),
+                    const SizedBox(height: 10),
+                    CustomButton(
+                        text: "Login",
+                        onTap: () {
+                          signInUser();
+                        }),
+                  ],
+                ),
+              ),
+            ),
+        ]),
+      ),
+    );
+  }
+}
